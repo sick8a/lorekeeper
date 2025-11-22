39979b711a488a0163cd0a76c3f5d3041f8c9aa0
Camyza <Camyza@users.noreply.github.com>
2025-11-22 04:57:49 +0000

    refactor: fix blade formatting

--- resources/views/admin/items/tags/foreground.blade.php
+++ resources/views/admin/items/tags/foreground.blade.php
@@ -2,17 +2,18 @@
     {!! Form::label('Image') !!}
 </h3>
 <p>
-    Upload an image for the foreground that will go in top a character's image. Notice that the image will need to be transparent for this to work.  You may stack multiple foregrounds on a character, provided it is transparent and follows the image size.
+    Upload an image for the foreground that will go in top a character's image. Notice that the image will need to be transparent for this to work. You may stack multiple foregrounds on a character, provided it is transparent and follows the image
+    size.
     This does not edit the images of the characters who have this item.
 </p>
 <p>
     It is highly recommended to only have one category with items that have this tag
 </p>
 <div class="col-8 mx-auto">
-    @if($tag->data && isset($tag->getData()['background-image']))
-        <img src="{{ url($tag->getData()['background-image']) }}" class="img-fluid mb-2"/>
+    @if ($tag->data && isset($tag->getData()['background-image']))
+        <img src="{{ url($tag->getData()['background-image']) }}" class="img-fluid mb-2" />
         {!! Form::file('image') !!}
     @else
-        {!! Form::file('image', ['required'=>'required']) !!}
+        {!! Form::file('image', ['required' => 'required']) !!}
     @endif
 </div>
--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -19,14 +19,14 @@
     @endif
 
     @include('character._header', ['character' => $character])
-      
+
     {{-- Main Image --}}
     <div class="row mb-3">
         <div class="col-md-7">
-            @if(isset($foreground) && is_array($foreground) && Config::get('lorekeeper.extensions.character_foregrounds.enabled'))
+            @if (isset($foreground) && is_array($foreground) && Config::get('lorekeeper.extensions.character_foregrounds.enabled'))
                 <div class="text-center" style="position: relative; display: inline-block;">
                     <!-- Foreground Overlays -->
-                    @foreach($foreground as $data)
+                    @foreach ($foreground as $data)
                         @php
                             // Read custom offset data
                             $css = $data['css_data'] ?? [];
@@ -51,19 +51,23 @@
                                 $customStyles .= "bottom: {$css['bottom-offset']}; ";
                             }
                         @endphp
-                        <div style="background-image: url('{{asset("images/data/items/foregrounds/" . $data['item_id'] . "/" . $data['tag_id'] . "/" . $data['tag_id'] . "-image.png") 
-                            }}'); position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-size: cover; background-repeat: no-repeat; z-index: {{ $loop->index + 2 }}; {{ $customStyles }};">
+                        <div
+                            style="background-image: url('{{ asset('images/data/items/foregrounds/' . $data['item_id'] . '/' . $data['tag_id'] . '/' . $data['tag_id'] . '-image.png') }}'); position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-size: cover; background-repeat: no-repeat; z-index: {{ $loop->index + 2 }}; {{ $customStyles }};">
                         </div>
                     @endforeach
-                    <a href="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists( public_path($character->image->imageDirectory.'/'.$character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}" data-lightbox="entry" data-title="{{ $character->fullName }}">
-                        <img src="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists( public_path($character->image->imageDirectory.'/'.$character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}" class="image" alt="{{ $character->fullName }}" />
+                    <a href="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}"
+                        data-lightbox="entry" data-title="{{ $character->fullName }}">
+                        <img src="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}"
+                            class="image" alt="{{ $character->fullName }}" />
                     </a>
                 </div>
             @else
-            {{-- Original Main Image --}}
+                {{-- Original Main Image --}}
                 <div class="text-center">
-                    <a href="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists( public_path($character->image->imageDirectory.'/'.$character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}" data-lightbox="entry" data-title="{{ $character->fullName }}">
-                        <img src="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists( public_path($character->image->imageDirectory.'/'.$character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}" class="image" alt="{{ $character->fullName }}" />
+                    <a href="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}"
+                        data-lightbox="entry" data-title="{{ $character->fullName }}">
+                        <img src="{{ $character->image->canViewFull(Auth::check() ? Auth::user() : null) && file_exists(public_path($character->image->imageDirectory . '/' . $character->image->fullsizeFileName)) ? $character->image->fullsizeUrl : $character->image->imageUrl }}"
+                            class="image" alt="{{ $character->fullName }}" />
                     </a>
                 </div>
             @endif

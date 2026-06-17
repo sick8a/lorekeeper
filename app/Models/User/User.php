2be9d6554145f313a34a434cf7964c4d34416161
sick8a <sick8a@users.noreply.github.com>
2026-06-17 18:21:35 +0000

    refactor: fix PHP styling

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -4,7 +4,9 @@ namespace App\Models\User;
 
 use App\Models\Character\Character;
 use App\Models\Character\CharacterBookmark;
+use App\Models\Character\CharacterDesignUpdate;
 use App\Models\Character\CharacterImageCreator;
+use App\Models\Character\CharacterTransfer;
 use App\Models\Comment\CommentLike;
 use App\Models\Currency\Currency;
 use App\Models\Currency\CurrencyLog;
@@ -16,20 +18,14 @@ use App\Models\Item\ItemLog;
 use App\Models\Notification;
 use App\Models\Rank\Rank;
 use App\Models\Recipe\Recipe;
+use App\Models\Report\Report;
 use App\Models\Shop\ShopLog;
 use App\Models\Submission\Submission;
 use App\Models\Theme;
+use App\Models\Trade;
 use App\Models\WorldExpansion\Faction;
 use App\Models\WorldExpansion\FactionRankMember;
 use App\Models\WorldExpansion\Location;
-use App\Models\Character\CharacterDesignUpdate;
-use App\Models\Character\CharacterTransfer;
-use App\Models\Rank\RankPower;
-use App\Models\Report\Report;
-use App\Models\Submission\SubmissionCharacter;
-use App\Models\User\UserCharacterLog;
-use App\Models\Trade;
-
 use App\Traits\Commenter;
 use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -1001,25 +997,25 @@ class User extends Authenticatable implements MustVerifyEmail {
         return $recipeCollection;
     }
 
-
     /**
-     * Check if there are any Admin Notifications
+     * Check if there are any Admin Notifications.
+     *
+     * @param mixed $user
      *
      * @return int
      */
-     public function hasAdminNotification($user)
-     {
+    public function hasAdminNotification($user) {
         $count = [];
-        $count[] = $this->hasPower('manage_submissions')    ? Submission::where('status', 'Pending')->whereNotNull('prompt_id')->count()    : 0; //submissionCount
-        $count[] = $this->hasPower('manage_submissions')    ? Submission::where('status', 'Pending')->whereNull('prompt_id')->count()       : 0; //claimCount
-        $count[] = $this->hasPower('manage_characters')     ? CharacterDesignUpdate::characters()->where('status', 'Pending')->count()      : 0; //designCount
-        $count[] = $this->hasPower('manage_characters')     ? CharacterDesignUpdate::myos()->where('status', 'Pending')->count()            : 0; //myoCount
-        $count[] = $this->hasPower('manage_characters')     ? CharacterTransfer::active()->where('is_approved', 0)->count()                 : 0; //transferCount
-        $count[] = $this->hasPower('manage_characters')     ? Trade::where('status', 'Pending')->count()                                    : 0; //tradeCount
-        $count[] = $this->hasPower('manage_characters')     ? Trade::where('status', 'Pending')->count()                                    : 0; //tradeCount
-        $count[] = $this->hasPower('manage_submissions')    ? GallerySubmission::pending()->collaboratorApproved()->count()                 : 0; //galleryCount
-        $count[] = $this->hasPower('manage_reports')        ? Report::where('status', 'Pending')->count()                                   : 0; //reportCount
-        $count[] = $this->hasPower('manage_reports')        ? Report::assignedToMe($this)->count()                                          : 0; //assignedReportCount
+        $count[] = $this->hasPower('manage_submissions') ? Submission::where('status', 'Pending')->whereNotNull('prompt_id')->count() : 0; // submissionCount
+        $count[] = $this->hasPower('manage_submissions') ? Submission::where('status', 'Pending')->whereNull('prompt_id')->count() : 0; // claimCount
+        $count[] = $this->hasPower('manage_characters') ? CharacterDesignUpdate::characters()->where('status', 'Pending')->count() : 0; // designCount
+        $count[] = $this->hasPower('manage_characters') ? CharacterDesignUpdate::myos()->where('status', 'Pending')->count() : 0; // myoCount
+        $count[] = $this->hasPower('manage_characters') ? CharacterTransfer::active()->where('is_approved', 0)->count() : 0; // transferCount
+        $count[] = $this->hasPower('manage_characters') ? Trade::where('status', 'Pending')->count() : 0; // tradeCount
+        $count[] = $this->hasPower('manage_characters') ? Trade::where('status', 'Pending')->count() : 0; // tradeCount
+        $count[] = $this->hasPower('manage_submissions') ? GallerySubmission::pending()->collaboratorApproved()->count() : 0; // galleryCount
+        $count[] = $this->hasPower('manage_reports') ? Report::where('status', 'Pending')->count() : 0; // reportCount
+        $count[] = $this->hasPower('manage_reports') ? Report::assignedToMe($this)->count() : 0; // assignedReportCount
 
         // If Adoption Center is installed:
         // $count[] = $this->hasPower('manage_submissions') && $this->hasPower('manage_characters') ? Surrender::where('status', 'Pending')->count() : 0; //surrenderCount
@@ -1028,6 +1024,5 @@ class User extends Authenticatable implements MustVerifyEmail {
         // $count[] = $this->hasPower('manage_affiliates')     ? Affiliate::where('status', 'Pending')->count()                                : 0; //affiliateCount
 
         return array_sum($count);
-     }
-
+    }
 }

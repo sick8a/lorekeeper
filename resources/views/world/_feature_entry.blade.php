d79e37c7a12dc1a13b98874dea1eb61c6b6d1adf
cicada <siiick8aaa@gmail.com>
2026-06-20 04:03:11 -0500

    fancy traits heh

--- resources/views/world/_feature_entry.blade.php
+++ resources/views/world/_feature_entry.blade.php
@@ -1,48 +1,74 @@
 <div class="row world-entry">
-    @if ($feature->has_image)
-        <div class="col-md-3 world-entry-image">
-            <a href="{{ $feature->imageUrl }}" data-lightbox="entry" data-title="{{ $feature->name }}">
-                <img src="{{ $feature->imageUrl }}" class="world-entry-image" alt="{{ $feature->name }}" />
-            </a>
-        </div>
-    @endif
-    <div class="{{ $feature->has_image ? 'col-md-9' : 'col-12' }}">
-        <x-admin-edit title="Trait" :object="$feature" />
-        <h3>
-            @if (!$feature->is_visible)
-                <i class="fas fa-eye-slash mr-1"></i>
-            @endif
-            {!! $feature->displayName !!}
-            <a href="{{ $feature->searchUrl }}" class="world-entry-search text-muted">
-                <i class="fas fa-search"></i>
-            </a>
-        </h3>
-        @if ($feature->feature_category_id)
-            <div>
-                <strong>Category:</strong> {!! $feature->category->displayName !!}
+    <div class="col-12">
+
+        <div class="card mb-3">
+            <div class="card-header">
+                <x-admin-edit title="Trait" :object="$feature" />
+
+                <h3 class="mb-0">
+                    @if (!$feature->is_visible)
+                        <i class="fas fa-eye-slash mr-1"></i>
+                    @endif
+
+                    {!! $feature->displayName !!}
+
+                    <a href="{{ $feature->searchUrl }}" class="world-entry-search text-muted">
+                        <i class="fas fa-search"></i>
+                    </a>
+                </h3>
             </div>
-        @endif
-        @if($feature->feature_subcategory_id)
-            <div><strong>Subcategory:</strong> {!! $feature->subcategory->displayName !!}</div>
-        @endif
-        @if ($feature->parent_id)
-            <div><strong>Parent Trait:</strong> {!! $feature->parent->displayName !!}</div>
-        @endif
-        @if ($feature->species_id)
-            <div>
-                <strong>Species:</strong> {!! $feature->species->displayName !!}
-                @if ($feature->subtype_id)
-                    ({!! $feature->subtype->displayName !!} subtype)
+
+            <div class="card-body">
+                @if ($feature->feature_category_id)
+                    <div>
+                        <strong>Category:</strong> {!! $feature->category->displayName !!}
+                    </div>
+                @endif
+
+                @if ($feature->feature_subcategory_id)
+                    <div>
+                        <strong>Subcategory:</strong> {!! $feature->subcategory->displayName !!}
+                    </div>
+                @endif
+
+                @if ($feature->parent_id)
+                    <div>
+                        <strong>Parent Trait:</strong> {!! $feature->parent->displayName !!}
+                    </div>
+                @endif
+
+                @if ($feature->species_id)
+                    <div>
+                        <strong>Species:</strong> {!! $feature->species->displayName !!}
+                        @if ($feature->subtype_id)
+                            ({!! $feature->subtype->displayName !!} subtype)
+                        @endif
+                    </div>
+                @endif
+
+                <div class="world-entry-text parsed-text mt-3">
+                    {!! $feature->parsed_description !!}
+                </div>
+
+                @if ($feature->has_image)
+                    <div class="text-center mt-4">
+                        <a href="{{ $feature->imageUrl }}"
+                           data-lightbox="entry"
+                           data-title="{{ $feature->name }}">
+                            <img src="{{ $feature->imageUrl }}"
+                                 class="img-fluid rounded"
+                                 style="max-height: 300px;"
+                                 alt="{{ $feature->name }}">
+                        </a>
+                    </div>
                 @endif
             </div>
-        @endif
-        <div class="world-entry-text parsed-text">
-            {!! $feature->parsed_description !!}
         </div>
+
         @if ($feature->altTypes->count())
             <div class="card mb-3">
                 <div class="card-header">
-                    <h5 class="inventory-header">
+                    <h5 class="inventory-header mb-0">
                         Variations
                     </h5>
                 </div>
@@ -50,21 +76,26 @@
                 <div class="card-body">
                     <div class="row">
                         @foreach ($feature->altTypes as $altType)
-                            <div class="col-md-3">
-
+                            <div class="col-md-6 mb-3">
                                 <div class="card h-100">
 
                                     @if ($altType->has_image)
-                                        <img src="{{ $altType->imageUrl }}"
-                                            class="card-img-top"
-                                            alt="{{ $feature->name }}">
+                                        <a href="{{ $altType->imageUrl }}"
+                                           data-lightbox="entry"
+                                           data-title="{{ $altType->name }}">
+                                            <img src="{{ $altType->imageUrl }}"
+                                                 class="card-img-top"
+                                                 style="height: 180px; object-fit: contain;"
+                                                 alt="{{ $altType->name }}">
+                                        </a>
                                     @endif
 
                                     <div class="card-body">
-
                                         <div class="mb-2">
                                             {!! $altType->displayName !!}
-                                            <a href="{{ $altType->searchUrl }}" class="world-entry-search text-muted">
+
+                                            <a href="{{ $altType->searchUrl }}"
+                                               class="world-entry-search text-muted">
                                                 <i class="fas fa-search"></i>
                                             </a>
                                         </div>
@@ -75,6 +106,7 @@
                                                 <div class="small mb-2">
                                                     <strong>Species:</strong>
                                                     {!! $altType->species->displayName !!}
+
                                                     @if ($altType->subtype_id)
                                                         ({!! $altType->subtype->displayName !!} subtype)
                                                     @endif
@@ -86,16 +118,15 @@
                                             </div>
 
                                         @endif
-
                                     </div>
 
                                 </div>
-
                             </div>
                         @endforeach
                     </div>
                 </div>
             </div>
         @endif
+
     </div>
 </div>

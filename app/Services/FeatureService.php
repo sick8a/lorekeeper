7d0b901a8cfe8d4d5601aafe031ecd88e09710ac
sick8a <sick8a@users.noreply.github.com>
2026-06-20 19:23:08 +0000

    refactor: fix PHP styling

--- app/Services/FeatureService.php
+++ app/Services/FeatureService.php
@@ -503,19 +503,19 @@ class FeatureService extends Service {
                 foreach ($data['alt']['id'] as $key=>$alt) {
                     // Collect data for the alt type
                     $altData[$key] = [
-                        'id'                  => $alt ? $alt : null,
-                        'parent_id'           => $feature->id,
-                        'feature_category_id' => $data['alt']['feature_category_id'][$key],
+                        'id'                     => $alt ? $alt : null,
+                        'parent_id'              => $feature->id,
+                        'feature_category_id'    => $data['alt']['feature_category_id'][$key],
                         'feature_subcategory_id' => $data['alt']['feature_subcategory_id'][$key] ?? null,
-                        'name'                => $data['alt']['name'][$key],
-                        'display_mode'        => $data['alt']['display_mode'][$key],
-                        'rarity_id'           => $data['alt']['rarity_id'][$key],
-                        'species_id'          => $data['alt']['species_id'][$key],
-                        'subtype_id'          => $data['alt']['subtype_id'][$key],
-                        'description'         => $alt ? $data['alt']['description'][$key] : null,
-                        'display_separate'    => $alt ? (isset($data['alt']['display_separate'][$key]) ? 1 : 0) : 1,
-                        'image'               => $data['alt']['image'][$key] ?? null,
-                        'remove_image'        => $alt ? (isset($data['alt']['remove_image'][$key]) ? 1 : 0) : 0,
+                        'name'                   => $data['alt']['name'][$key],
+                        'display_mode'           => $data['alt']['display_mode'][$key],
+                        'rarity_id'              => $data['alt']['rarity_id'][$key],
+                        'species_id'             => $data['alt']['species_id'][$key],
+                        'subtype_id'             => $data['alt']['subtype_id'][$key],
+                        'description'            => $alt ? $data['alt']['description'][$key] : null,
+                        'display_separate'       => $alt ? (isset($data['alt']['display_separate'][$key]) ? 1 : 0) : 1,
+                        'image'                  => $data['alt']['image'][$key] ?? null,
+                        'remove_image'           => $alt ? (isset($data['alt']['remove_image'][$key]) ? 1 : 0) : 0,
                     ];
 
                     // If the ID is already set, modify the existing feature

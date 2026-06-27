9c471aaae0f6ab9f9e82711162bd795bb3a3a3f0
sick8a <sick8a@users.noreply.github.com>
2026-06-27 04:14:37 +0000

    refactor: fix PHP styling

--- database/migrations/2026_06_27_013927_add_phenotype_to_characters_table.php
+++ database/migrations/2026_06_27_013927_add_phenotype_to_characters_table.php
@@ -4,15 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::table('characters', function (Blueprint $table) {
             $table->string('phenotype', 300)->nullable()->after('description');
         });
@@ -20,11 +16,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::table('characters', function (Blueprint $table) {
             $table->dropColumn('phenotype');
         });
